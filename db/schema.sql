bd269ebc2ddc7895fb139a0772e889031cfbcb7b
Nadzeya Zianiuk <nadzeya@Nadzeyas-MacBook-Air.local>
2026-07-05 20:15:53 -0400

    Database - part 1

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,21 @@
 -- TODO
+-- Write db/schema.sql to create tables according to the schema above.
+-- The name column of folders is unique.
+-- The combination of name and folder_id of files is unique. (see docs!)
+-- There is a one-many relation: one folder can have many files.
+-- Folder deletion should cascade to all related files.
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE folders(
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE files(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    size INTEGER NOT NULL,
+    folder_id INTEGER NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+    UNIQUE (name, folder_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createFolder } from "./queries/folders.js";
+import { createFile } from "./queries/files.js";
 
 await db.connect();
 await seed();
@@ -7,4 +10,14 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  for (let i = 0; i < 3; i++) {
+    const name = faker.lorem.word();
+    const folder = await createFolder({ name });
+    const folderID = folder.id;
+    for (let y = 0; y < 5; y++) {
+      const name = faker.lorem.word();
+      const size = faker.number.int({ min: 10, max: 100 });
+      await createFile({ name, size, folderID });
+    }
+  }
 }
